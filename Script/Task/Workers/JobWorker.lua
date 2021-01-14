b1fecd130271ff5b94b48c8bd5fd4cbe24ed1c12
xingfanou <757609414@qq.com>
2021-01-14 14:44:16 +0800

    优化代码

--- Script/Task/Workers/JobWorker.lua
+++ Script/Task/Workers/JobWorker.lua
@@ -209,7 +209,7 @@ function DeployServer.HandleCreateProgressWorker(sender, msg)
 			cmd = cmd .. "cd \"" .. detail.batch_dir .. "\" && "
 		end
 	end
-	cmd = cmd .. "start " .. detail.batch_cmd .. " " .. detail.batch_param .. " && exit"
+	cmd = cmd .. "start " .. detail.batch_cmd .. " " .. detail.batch_param
 	os.execute(cmd)
 	return {}
 end
