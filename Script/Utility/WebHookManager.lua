68c9a34835cd931ce7ae283dedae325d1ada79fc
ALittleSail <757609414@qq.com>
2021-01-19 21:22:22 +0800

    github和svn分协议处理

--- Script/Utility/WebHookManager.lua
+++ Script/Utility/WebHookManager.lua
@@ -7,30 +7,49 @@ local ALittle = ALittle
 local ___pairs = pairs
 local ___ipairs = ipairs
 
-ALittle.RegStruct(2100150852, "DeployServer.AWebHook", {
-name = "DeployServer.AWebHook", ns_name = "DeployServer", rl_name = "AWebHook", hash_code = 2100150852,
-name_list = {},
-type_list = {},
-option_map = {}
-})
 ALittle.RegStruct(1363542185, "DeployServer.WebHookRepository", {
 name = "DeployServer.WebHookRepository", ns_name = "DeployServer", rl_name = "WebHookRepository", hash_code = 1363542185,
 name_list = {"html_url"},
 type_list = {"string"},
 option_map = {}
 })
-ALittle.RegStruct(1314669887, "DeployServer.QWebHook", {
-name = "DeployServer.QWebHook", ns_name = "DeployServer", rl_name = "QWebHook", hash_code = 1314669887,
+ALittle.RegStruct(970880527, "DeployServer.AWebHookByGithub", {
+name = "DeployServer.AWebHookByGithub", ns_name = "DeployServer", rl_name = "AWebHookByGithub", hash_code = 970880527,
+name_list = {},
+type_list = {},
+option_map = {}
+})
+ALittle.RegStruct(884914724, "DeployServer.QWebHookBySvn", {
+name = "DeployServer.QWebHookBySvn", ns_name = "DeployServer", rl_name = "QWebHookBySvn", hash_code = 884914724,
+name_list = {},
+type_list = {},
+option_map = {}
+})
+ALittle.RegStruct(-161157926, "DeployServer.QWebHookByGithub", {
+name = "DeployServer.QWebHookByGithub", ns_name = "DeployServer", rl_name = "QWebHookByGithub", hash_code = -161157926,
 name_list = {"repository"},
 type_list = {"DeployServer.WebHookRepository"},
 option_map = {}
 })
+ALittle.RegStruct(-119002459, "DeployServer.AWebHookBySvn", {
+name = "DeployServer.AWebHookBySvn", ns_name = "DeployServer", rl_name = "AWebHookBySvn", hash_code = -119002459,
+name_list = {},
+type_list = {},
+option_map = {}
+})
 
-function DeployServer.HandleQWebHook(sender, msg)
+function DeployServer.HandleQWebHookByGithub(sender, msg)
 	local ___COROUTINE = coroutine.running()
 	g_TaskManager:StartTaskByWebHook(msg.repository.html_url)
 	return {}
 end
 
-ALittle.RegHttpCallback("DeployServer.QWebHook", DeployServer.HandleQWebHook)
+ALittle.RegHttpCallback("DeployServer.QWebHookByGithub", DeployServer.HandleQWebHookByGithub)
+function DeployServer.HandleQWebHookBySvn(sender, msg)
+	local ___COROUTINE = coroutine.running()
+	g_TaskManager:StartTaskByWebHook(sender.url)
+	return {}
+end
+
+ALittle.RegHttpCallback("DeployServer.QWebHookBySvn", DeployServer.HandleQWebHookBySvn)
 end
